7b798be788fac12d9afe0b3ce4ecde309f891d8b
Vinxont <vin.yizhe.h@gmail.com>
2019-11-12 21:55:46 +0800

    sdwa

---
+++ Powerpuff Girls Detective Game/Assets/scripts/Clickable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour
+{
+    [SerializeField] private GameObject objlock;
+    [SerializeField] private GameObject objtext;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("on mouse down");
+        //add the image 
+        objlock.SetActive(true);
+        objtext.SetActive(true);
+    }
+}
---
+++ Powerpuff Girls Detective Game/Assets/scripts/InputScale.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputScale : MonoBehaviour
+{[SerializeField] private Text input_text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            if (input_text.text.Equals("8527")){
+                Debug.Log("yessss");
+            }
+            else Debug.Log("no");
+            this.gameObject.SetActive(false);
+        }
+    }
+}
